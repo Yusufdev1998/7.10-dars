538b1f07bbf875e81f58b49681ae5b896bf861be
Yusufdev1998 <yusufdev1998@gmail.com>
2023-08-09 17:58:34 +0500

    token changed

--- src/api/customAxios.js
+++ src/api/customAxios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const customAxios = axios.create({
   headers: {
-    Authorization: `Bearer ${import.meta.env.VITE_TOKEN}`,
+    Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhY2Y5NWUzYzkyMDQ4NTY4ZmU4MTA3YTMyOGE1Yjk0YyIsInN1YiI6IjY0Y2JmMmM0NzA2ZTU2MDEwYjBhZjNhOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.iiv1pvzvxcx63UdnUDuz5CbxCtC4Lw_CrPp7g4XA-fE`,
   },
 });
 
